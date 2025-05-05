2f4c1a3d4316ec9ecddf856ffec75156983e8871
Fred Mwita <mwitamtana0@gmail.com>
2025-05-05 18:06:04 +0300

    Initial Commit

---
+++ src/main/java/com/example/temperaturemonitoringsystem/service/TemperatureMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.temperaturemonitoringsystem.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureMonitoringSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemperatureMonitoringSystemApplication.class, args);
+    }
+
+}
